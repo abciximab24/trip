eb2b39f14ba6f7e7bb8e265d1461b410c61a8e51
Wilson <qpzm620@gmail.com>
2026-02-04 22:48:03 +0800

    Remove redundant Google Maps script load from layout, rely on dynamic importLibrary loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places,geometry`}
-          strategy="beforeInteractive"
-        />
         {children}
       </body>
     </html>
